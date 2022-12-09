590eb3e64929748bc89fdc81d0942df6a9b79d1b
anhishek1289@gmail.com <abhishek1289@gmail.com>
2022-12-09 20:58:16 +0530

    jsnew

--- main.js
+++ main.js
@@ -24,12 +24,42 @@ let studentRecords = [ {name: 'John', id: 123, marks : 98 },
 // let id = studentRecords.filter(studentRecords=> studentRecords.id>120).reduce(((num3, num4) => num3 + num4.marks),0);
 // console.log(id);
 
-let arrnew = studentRecords.map(function(studentRecords){
-    if(studentRecords.marks < 50){
-        studentRecords.marks += 15;
-        return studentRecords;
-}
+// let arrnew = studentRecords.map(function(stu){
+//     if(stu.marks < 50){
+//         stu.marks += 15;
+        
+// }return stu; 
+// }).filter(stu => stu.marks > 50).reduce(((num1, num2) => num1+=num2.marks), 0);
+
+
+let res6 = studentRecords.map (function(mark){
+    if(mark.marks < 50){
+        mark.marks +=15
+    }
+    return mark;
+}).filter(c=>c.marks>50).reduce((acc,cur)=>acc+=cur.marks,0)
+console.log(res6);
+// let stunew = function(stu)
     
-}).filter(studentRecords => studentRecords.marks > 50).reduce((num1, num2) => num1+num2.marks, 0);
+// }
+// let stuarray= [];
+// function Student(name, classname, rollno){
+//     this.stuname = name;
+//     this.stuname = classname
+//     this.sturollno= rollno
+// }
+// let student1 = new Student("EA17", "sept", 1)
+// let student2 = new Student("EA15", "july", 2)
+// let student3 = new Student("EA13", "june", 3)
+// let student4 = new Student("EA12", "nov", 4)
+// let student5 = new Student("EA19", "oct", 5)
+// let student6 = new Student("EA20", "aug", 6)
 
+// stuarray.push(student1)
+// stuarray.push(student2)
+// stuarray.push(student3)
+// stuarray.push(student4)
+// stuarray.push(student5)
+// stuarray.push(student6)
 
+//
